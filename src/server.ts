a8e10a0f37d0083b3aefd27023fefa1d8a1f7b0d
Christian Bayer <christian.bayer@universo.univates.br>
2023-09-04 23:50:12 -0300

    fix: remove helmet

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import "dotenv/config";
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
-import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
 
 const server = express();
@@ -13,14 +12,6 @@ server.use(rateLimit({
   windowMs: 1 * 60 * 1000,
   max: 20,
 }));
-
-server.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
-    },
-  }),
-);
 
 server.use(routes);
 
