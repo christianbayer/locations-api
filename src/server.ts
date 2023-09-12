5ad00b700abaec9ab34a7c9171b0670c647d6991
Christian Bayer <christian.bayer@universo.univates.br>
2023-09-11 23:36:57 -0300

    fix: cors issues

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,15 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 
+const port = process.env.EXPRESS_PORT || 3000;
 const server = express();
 
-server.use(cors());
+server.use(cors({ origin: '*' }));
+
+server.disable('x-powered-by');
 
 server.use(routes);
 
-server.listen(process.env.EXPRESS_PORT || 3000);
+server.listen(port, () => {
+  console.log(`Litening on port ${port}!`);
+});
